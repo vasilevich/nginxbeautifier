f3ece95aebe3ee6e36dd3b03c01052ca1a2bff65
yosef langer <yosef@doar.systems>
2018-12-04 22:56:51 +0200

    fixed the issue mentioned here: https://github.com/vasilevich/nginxbeautifier/issues/9

--- nginxbeautifier.js
+++ nginxbeautifier.js
@@ -213,24 +213,30 @@ function clean_lines(configContents) {
             newline = 0;
             var line = splittedByLines[index] = strip_line(splittedByLines[index]);
             if (line != "}" && line != "{" && !(line.includes("('{") || line.includes("}')"))) {
-                var i = line.indexOf("}");
-                if (i >= 0) {
-                    splittedByLines[index] = strip_line(line.slice(0, i - 1));
-                    splittedByLines.insert(index + 1, "}");
-                    var l2 = strip_line(line.slice(i + 1));
+                var startOfComment = line.indexOf("#");
+                var comment = startOfComment >= 0 ? line.slice(startOfComment) : "";
+                var code = startOfComment >= 0 ? line.slice(0, startOfComment) : line;
+                var startOfParanthesis = code.indexOf("}");
+                if (startOfParanthesis >= 0) {
+                    if (startOfParanthesis > 0) {
+                        splittedByLines[index] = strip_line(code.slice(0, startOfParanthesis - 1));
+                        splittedByLines.insert(index + 1, "}");
+                    }
+                    var l2 = strip_line(code.slice(startOfParanthesis + 1));
                     if (l2 != "")
                         splittedByLines.insert(index + 2, l2);
-                    line = splittedByLines[index];
+                    code = splittedByLines[index];
                 }
-                i = line.indexOf("{");
-                if (i >= 0) {
-                    splittedByLines[index] = strip_line(line.slice(0, i));
+                var endOfParanthesis = code.indexOf("{");
+                if (endOfParanthesis >= 0) {
+                    splittedByLines[index] = strip_line(code.slice(0, endOfParanthesis));
                     splittedByLines.insert(index + 1, "{");
-                    var l2 = strip_line(line.slice(i + 1));
+                    var l2 = strip_line(code.slice(endOfParanthesis + 1));
                     if (l2 != "")
                         splittedByLines.insert(index + 2, l2);
 
                 }
+                line = code;
             }
         }
         //remove more than two newlines
@@ -265,6 +271,7 @@ function join_opening_bracket(lines) {
 }
 
 var INDENTATION = '\t';
+var options = {INDENTATION};
 
 function perform_indentation(lines) {
     var indented_lines, current_indent, line;
@@ -278,7 +285,7 @@ function perform_indentation(lines) {
             current_indent -= 1;
         }
         if (line !== "") {
-            indented_lines.push(INDENTATION.repeat(current_indent) + line);
+            indented_lines.push(options.INDENTATION.repeat(current_indent) + line);
         } else {
             indented_lines.push("");
         }
@@ -341,268 +348,24 @@ function walkSync(dir, ext, filelist) {
     return filelist;
 };
 
-//define nodejs modules
-var fs = require('fs');
-var path = require('path');
-
-var options = {
-        name: "nginxbeautifier",
-        spaces: 0,
-        tabs: 0,
-        dontJoinCurlyBracet: false,
-        align: false,
-        trailingBlankLines: false,
-        recursive: false,
-        inputPath: [],
-        outputPath: [],
-        extension: "conf"
-};
-
-
-var knownArguments = {
-        "--help": function (input) {
-            if (input == "desc")
-                return "Show this help text.";
-
-            console.log("Usage: " + options.name + " [OPTION]... [FILE]...");
-            console.log("Description: Formats nginx conf files into a more readable format by re-indenting the lines.");
-            console.log("");
-            console.log("Mandatory arguments to long options are mandatory for short options too, Arguments are case insensitive.");
-            var argPrint = {};
-            for (var key in knownArguments) {
-                var desc = knownArguments[key]("desc");
-                if (argPrint[desc] == null)
-                    argPrint[desc] = '';
-                argPrint[desc] = key + ", " + argPrint[desc];
-            }
-            for (var desc in argPrint) {
-                console.log(argPrint[desc], desc);
-            }
-            console.log("");
-            console.log("Usage examples:");
-            console.log("(1)>" + options.name + " -s 4 -r sites-enabled/");
-            console.log("(2)>" + options.name + " -s 4 -r /etc/nginx/sites-enabled/");
-            console.log("(3)>" + options.name + " -s 4 -i /etc/nginx/sites-enabled/site.conf -o /etc/nginx/sites-enabled/newSite.conf ");
-            console.log("(4)>" + options.name + " -s 4 -i /etc/nginx/sites-enabled/site.conf");
-            console.log("(5)>" + options.name + " -s 4 -i /etc/nginx/sites-enabled/*");
-            console.log("(6)>" + options.name + " -t 4 -i /etc/nginx/sites-enabled/*");
-            console.log("(7)>" + options.name + " -t 4 /etc/nginx/sites-enabled/*");
-            console.log("(8)>" + options.name + " -t 4 -i /etc/nginx/sites-enabled/* -o /etc/nginx/new-sites-enabled/*");
-            return false;
-
-        },
-
-        "--space": function (number) {
-            if (number == "desc")
-                return "Amount of spaces to indent with, Can not be used if tabs are specified."
-            else if (isNaN(number)) {
-                knownArguments["--space"](1);
-                return true;
-            }
-            if (options.tabs > 0) {
-                console.log("Error! tabs were already defined, please choose one or the other.")
-                knownArguments["--help"]();
-                process.exit();
-            }
-            options.spaces = parseInt(number);
-            INDENTATION = " ".repeat(options.spaces);
-
-        },
-        "--tabs": function (number) {
-            if (number == "desc")
-                return "Amount of tabs to indent with, Can not be used if spaces are specified."
-            else if (isNaN(number)) {
-                knownArguments["--tabs"](1);
-                return true;
-            }
-            if (options.spaces > 0) {
-                console.log("Error! spaces were already defined, please choose one or the other.")
-                knownArguments["--help"]();
-                process.exit();
-            }
-            options.tabs = parseInt(number);
-            INDENTATION = "\t".repeat(options.tabs);
-        },
-        "--blank-lines": function (input) {
-            if (input == "desc") 
-                return "if set to true, an empty line will be inserted after opening brackets";
-            options.trailingBlankLines = true;
-        },
-        "--dont-join": function (input) {
-            if (input == "desc")
-                return "if set to true, commands such as 'server' and '{' will be on a seperate line, false by default ('server {' )";
-            options.dontJoinCurlyBracet = true;
-        }
-        ,
-        "--align": function (input) {
-            if (input == "desc")
-                return "if set to true, all applicable attribute values will be vertically aligned with each other";
-            options.align = true;
-        }
-        ,
-        "--recursive": function (input) {
-            if (input == "desc")
-                return "scan the whole current folder, and all sub folders recursively.";
-            options.recursive = true;
-            return false;
-        }
-        ,
-        "--input": function (file) {
-            var recursive = false;
-            if (file == "desc")
-                return "The file to input, is optional if you provide a path after all the arguments.";
-            if (file.startsWith("-"))
-                return true;
-            if (file.endsWith("*")) {
-                //knownArguments["--recursive"]();
-                recursive = true;
-                file = file.slice(0, file.length - 1);
-                file = file.length > 0 ? file : ".";
-            }
-            options.inputPath.push({name: file, recursive: recursive});
-        }
-        ,
-        "--output": function (file) {
-            var recursive = false;
-            if (file == "desc")
-                return "The file to output to, is optional if you provide a path after all the arguments.";
-            if (file.startsWith("-"))
-                return true;
-            if (file.endsWith("*")) {
-                // knownArguments["--recursive"]();
-                recursive = true;
-                file = file.slice(0, file.length - 1);
-                file = file.length > 0 ? file : ".";
-            }
-            options.outputPath.push({name: file, recursive: recursive});
-        },
-        "--extension": function (ext) {
-            if (ext == "desc")
-                return "The extension of the config file to look for(.conf by default).";
-            if (ext.startsWith("-"))
-                return true;
-            options.extension = ext;
-        }
-
-    }
-;
-//shortcuts
-knownArguments["-h"] = knownArguments["--help"];
-knownArguments["-s"] = knownArguments["--space"];
-knownArguments["-t"] = knownArguments["--tabs"];
-knownArguments["-r"] = knownArguments["--recursive"];
-knownArguments["-i"] = knownArguments["--input"];
-knownArguments["-o"] = knownArguments["--output"];
-knownArguments["-bl"] = knownArguments["--blank-lines"];
-knownArguments["--dontjoin"] = knownArguments["--dont-join"];
-knownArguments["-dj"] = knownArguments["--dont-join"];
-knownArguments["-a"] = knownArguments["--align"];
-knownArguments["-ext"] = knownArguments["--extension"];
-knownArguments["-e"] = knownArguments["--extension"];
-var wasFunc = null;
-if (process.argv.length > 2) {
-    for (var key in process.argv) {
-        if (key >= 2) {
-            var arg = process.argv[key].toLowerCase();
-            if (arg.startsWith("-")) {
-                var argFunc = knownArguments[arg];
-                if (argFunc != null) {
-                    //if its true, excpecting another arg next iteration
-                    if (argFunc(arg))
-                        wasFunc = argFunc;
-                }
-            }
-            else if (!isNaN(arg) && wasFunc != null) {
-                wasFunc(arg);
-                wasFunc = null;
-            }
-            else {
-                //its probably a file path
-                knownArguments["-i"](process.argv[key]);
-            }
-        }
-    }
-}
-else {
-    console.log("Error! no arguments were provided. I don't know what to do!")
-    knownArguments["--help"]();
-    process.exit()
-}
-/*
- if (!options.recursive) {
- if (!options.inputPath.endsWith(options.extension)) {
- console.log("Error! folder was selected, but no recursive option has been activated.")
- knownArguments["--help"]();
- process.exit();
- }
- }
- //options.outputPath.endsWith(options.extension)&&
- else if (!fs.statSync(options.outputPath).isDirectory()) {
- console.log("Error! input is recursive(multiple files), output is a single file? this makes no sense!")
- knownArguments["--help"]();
- process.exit();
- }
+/**
+ * option1:  INDENTATION :  '\t'
+ * @param inputOptions
  */
-
-//if we got up here, all seems fine
-
-
-//lets fetch the list of all the relevant files
-var filesArr = [];
-
-for (var index = 0, length = options.inputPath.length; index < length; index++) {
-    if (options.inputPath[index] != "") {
-        if ((options.inputPath[index].recursive || options.recursive) && fs.statSync(options.inputPath[index].name).isDirectory()) {
-            filesArr = filesArr.concat(walkSync(options.inputPath[index].name, options.extension));
-        }
-        else if (fs.statSync(options.inputPath[index].name).isFile()) {
-            filesArr = filesArr.concat(options.inputPath[index].name);
-        }
-        else {
-            console.log("Error! folder was selected, but no recursive option(path/* or -r) has been activated.")
-            knownArguments["--help"]();
-            process.exit();
-        }
+function modifyOptions(inputOptions) {
+    for (var k in inputOptions) {
+        options[k] = inputOptions[k];
     }
 }
 
-
-for (var index = 0, length = filesArr.length; index < length; index++) {
-    var file = filesArr[index];
-    console.log("Working on file: " + file);
-    var fileContents = fs.readFileSync(file, "utf8");
-    //splti the file into lines, clean spaces
-    var cleanLines = clean_lines(fileContents);
-    //join opening bracket(if user wishes so) true by default
-    if (!options.dontJoinCurlyBracet)
-        cleanLines = join_opening_bracket(cleanLines);
-    //perform the indentation
-    cleanLines = perform_indentation(cleanLines);    
-    // vertically align all eligible declarations
-    if (options.align) {
-        cleanLines = perform_alignment(cleanLines);
-    }
-
-    //combine all the lines back together
-    var outputContents = cleanLines.join("\n");
-    //save all the contents to the file.
-    //if the user didnt choose output path, then the input file is used.
-    //  if (options.inputPath == options.outputPath) {
-    if (options.outputPath.length > 0)
-        fs.writeFileSync(options.outputPath[0] + "/" + file, outputContents, 'utf8');
-    else
-        fs.writeFileSync(file, outputContents, 'utf8');
-    // }
-    //else we have to write to the chosen output file or folder
-    //   else {
-
-    //   }
-
-
+if (module) {
+    module.exports = {
+        walkSync,
+        perform_alignment,
+        perform_indentation,
+        join_opening_bracket,
+        clean_lines,
+        modifyOptions,
+        strip_line
+    };
 }
-
-
-console.log("Success.");
-
-
-//console.log(finalOutput);
